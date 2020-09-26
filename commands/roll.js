9004cbdd7af22fda0e6cbf13ae75b1525a6de2ac
SparrowDivision <71931496+SparrowDivision@users.noreply.github.com>
2020-09-26 20:13:10 +0200

    Update roll.js

--- commands/roll.js
+++ commands/roll.js
@@ -1,5 +1,7 @@
-function getRndInteger(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) ) + min;
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 module.exports = {
