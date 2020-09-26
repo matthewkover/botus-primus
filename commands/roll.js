8233fc13d4a6d7363747772bd96558b61d3431a8
SparrowDivision <71931496+SparrowDivision@users.noreply.github.com>
2020-09-26 20:22:25 +0200

    Update roll.js

--- commands/roll.js
+++ commands/roll.js
@@ -11,9 +11,9 @@ module.exports = {
         var temp = message.content.split(" ");
         if (temp.length == 1) {
             message.reply(" rolled " + "**" + getRandomInt(1, 6) + "**.");
-        } else if (temp.length == 2 && Number.isInteger(temp[1])) {
+        } else if (temp.length == 2 && Number.isInteger(temp[1]/1)) {
             message.reply(" rolled " + "**" + getRandomInt(1, temp[1]) + "**.");
-        } else if (temp.length == 3 && Number.isInteger(temp[1]) && Number.isInteger(temp[2])) {
+        } else if (temp.length == 3 && Number.isInteger(temp[1]/1) && Number.isInteger(temp[2]/1)) {
             message.reply(" rolled " + "**" + getRandomInt(temp[1], temp[2]) + "**.");
         } else {
             message.channel.send("Wrong input!");
