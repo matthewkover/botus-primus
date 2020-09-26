a6f5b3632a30511f213917986615abfe108b586a
SparrowDivision <71931496+SparrowDivision@users.noreply.github.com>
2020-09-26 20:56:00 +0200

    Update roll.js
    Roll comand Final version

--- commands/roll.js
+++ commands/roll.js
@@ -11,13 +11,13 @@ module.exports = {
         var temp = message.content.split(" ");
         message.delete();
         if (temp.length == 1) {
-            message.reply("rolled " + "**" + getRandomInt(1, 6) + "**.");
+            message.reply("rolled " + "**" + getRandomInt(1, 6) + "**.").then(d => {d.delete({timeout: 1000 * 60 * 5})});
         } else if (temp.length == 2 && Number.isInteger(temp[1]/1)) {
-            message.reply("rolled " + "**" + getRandomInt(1, temp[1]) + "**.");
+            message.reply("rolled " + "**" + getRandomInt(1, temp[1]) + "**.").then(d => {d.delete({timeout: 1000 * 60 * 5})});
         } else if (temp.length == 3 && Number.isInteger(temp[1]/1) && Number.isInteger(temp[2]/1)) {
-            message.reply("rolled " + "**" + getRandomInt(temp[1], temp[2]) + "**.");
+            message.reply("rolled " + "**" + getRandomInt(temp[1], temp[2]) + "**.").then(d => {d.delete({timeout: 1000 * 60 * 5})});
         } else {
-            message.channel.send("**Wrong input!**").then(d => {d.delete({timeout: 1000 * 60 * 3})});;
+            message.channel.send("**Wrong input!**").then(d => {d.delete({timeout: 1000 * 60 * 5})});
         }
     }
 }
