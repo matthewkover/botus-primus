fcd21761647aa0534ea2f5bf515202b0998cb359
SparrowDivision <71931496+SparrowDivision@users.noreply.github.com>
2020-09-26 20:05:05 +0200

    Update roll.js

--- commands/roll.js
+++ commands/roll.js
@@ -1,10 +1,14 @@
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
 module.exports = {
     name: 'roll',
     description: 'This is a command rolls a dice.',
     execute(message, args) {
         var temp = message.content.split(" ");
         if (temp.length == 1) {
-            message.reply(" rolled " + "**" + (Math.floor(Math.random() * 10) + 1) + "**.");
+            message.reply(" rolled " + "**" + getRndInteger(1, 6) + "**.");
         } else if (temp.length == 2) {
             try {
                 if (temp[1] % 1 == 0) {
