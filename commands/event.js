15a4af623ee5bb0a7cb9caa92926582d97339642
asdkhaa <30670722+asdkhaa@users.noreply.github.com>
2020-09-26 15:56:45 +0200

    event.js card test

--- commands/event.js
+++ commands/event.js
@@ -3,7 +3,17 @@ module.exports = {
     description: 'Event reminders can be set by this command',
     execute(message, args, Discord) {
         const Embed = new Discord.MessageEmbed()
-        
+        .setAuthor('EVENT_TITLE')
+        .setDescription('EVENT_DESCRIPTION')
+        .addField('Time', 'TIME', true)
+        .addFields (
+            { name: 'Accepted', value: 'ACCEPTED_USERS'},
+            { name: 'Declined', value: 'DECLINED_USERS'},
+            { name: 'Tentative', value: 'TENTATIVE_USERS'},
+        )
+        .setFooter('Created by xyz')
+        .setTimestamp()
+        ;
         message.channel.send(Embed);
     }
 }
