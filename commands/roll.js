b56336212823b2c629a07d6da7da4f62d0d04989
Mátyás Fehér <71931496+SparrowDivision@users.noreply.github.com>
2020-10-05 23:18:18 +0200

    Update roll.js

--- commands/roll.js
+++ commands/roll.js
@@ -8,14 +8,15 @@ module.exports = {
     name: 'roll',
     description: 'This command rolls a dice.',
     execute(message, args) {
+        var tout = 300000; //5 mins
         var temp = message.content.split(" ");
         message.delete();
         if (temp.length == 1)
-            message.reply("rolled " + "**" + getRandomInt(1, 6) + "**.").then(d => {d.delete({timeout: 1000 * 60 * 5})});
+            message.reply("rolled " + "**" + getRandomInt(1, 6) + "**.").then(d => {d.delete({timeout: tout})});
         else if (temp.length == 2 && Number.isInteger(temp[1]/1))
-            message.reply("rolled " + "**" + getRandomInt(1, temp[1]) + "**.").then(d => {d.delete({timeout: 1000 * 60 * 5})});
+            message.reply("rolled " + "**" + getRandomInt(1, temp[1]) + "**.").then(d => {d.delete({timeout: tout})});
         else if (temp.length == 3 && Number.isInteger(temp[1]/1) && Number.isInteger(temp[2]/1))
-            message.reply("rolled " + "**" + getRandomInt(temp[1], temp[2]) + "**.").then(d => {d.delete({timeout: 1000 * 60 * 5})});
+            message.reply("rolled " + "**" + getRandomInt(temp[1], temp[2]) + "**.").then(d => {d.delete({timeout: tout})});
         else
             message.channel.send("**Wrong input!**").then(d => {d.delete({timeout: 1000 * 60 * 5})});
     }
