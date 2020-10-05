4666eb5c7a919529c7999b0a4e1ebe50df2cba5b
Mátyás Fehér <71931496+SparrowDivision@users.noreply.github.com>
2020-10-05 23:05:06 +0200

    Update roll.js

--- commands/roll.js
+++ commands/roll.js
@@ -10,14 +10,13 @@ module.exports = {
     execute(message, args) {
         var temp = message.content.split(" ");
         message.delete();
-        if (temp.length == 1) {
+        if (temp.length == 1)
             message.reply("rolled " + "**" + getRandomInt(1, 6) + "**.").then(d => {d.delete({timeout: 1000 * 60 * 5})});
-        } else if (temp.length == 2 && Number.isInteger(temp[1]/1)) {
+        else if (temp.length == 2 && Number.isInteger(temp[1]/1))
             message.reply("rolled " + "**" + getRandomInt(1, temp[1]) + "**.").then(d => {d.delete({timeout: 1000 * 60 * 5})});
-        } else if (temp.length == 3 && Number.isInteger(temp[1]/1) && Number.isInteger(temp[2]/1)) {
+        else if (temp.length == 3 && Number.isInteger(temp[1]/1) && Number.isInteger(temp[2]/1))
             message.reply("rolled " + "**" + getRandomInt(temp[1], temp[2]) + "**.").then(d => {d.delete({timeout: 1000 * 60 * 5})});
-        } else {
+        else
             message.channel.send("**Wrong input!**").then(d => {d.delete({timeout: 1000 * 60 * 5})});
-        }
     }
 }
