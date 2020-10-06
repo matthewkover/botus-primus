bc1c9c7ac16346cc4b03ca92bc13cf218a1ce479
Mátyás Fehér <71931496+SparrowDivision@users.noreply.github.com>
2020-10-06 11:43:33 +0200

    Update event.js

--- commands/event.js
+++ commands/event.js
@@ -1,47 +1,63 @@
 class Event {
-    constructor(id, ename, date, acc, dec, tent) {
+    constructor(id, ename, date, acc, dec, tent, mb, dm) {
         this.eventId = id;
         this.eventName = ename;
         this.eventDate = date;
         this.accepted = acc;
         this.declined = dec;
         this.tentative = tent;
+        this.madeBy = mb;
+        this.dateMade = dm;
     }
-    
-    checkInt(yy, mm, dd, hh, min) {
-        if (!Number.isInteger(yy / 1) || !Number.isInteger(mm / 1) || !Number.isInteger(dd / 1) || !Number.isInteger(hh / 1) || !Number.isInteger(min / 1)) 
-            return false;
-        return true;
-    }
-    
-    splitMessage(input) {
-        for (var i = 0; i < input.length; i++) {
-            if (input[i] == '.' || input[i] == ':') 
-                input = input.substring(0, i) + ' ' + input.substring(i + 1);
-        }
-        input = input.split(" ");
-        input.shift();
-        return input;
-    }
-    
-    getEventName(tomb) {
-        var event = "";
-        for (var i = 0; i < tomb.length; i++) {
-            if (i == 0)
-                event = tomb[i];
-            else
-                event = event + " " + tomb[i];
-        }
-        return event;
-   } 
 }
 
+function checkInt(yy, mm, dd, hh, min) {
+    if (!Number.isInteger(yy / 1) || !Number.isInteger(mm / 1) || !Number.isInteger(dd / 1) || !Number.isInteger(hh / 1) || !Number.isInteger(min / 1)) 
+        return false;
+    return true;
+}
+
+function splitMessage(input) {
+    for (var i = 0; i < input.length; i++) {
+        if (input[i] == '.' || input[i] == ':') 
+            input = input.substring(0, i) + ' ' + input.substring(i + 1);
+    }
+    input = input.split(" ");
+    input.shift();
+    return input;
+}
+
+function getEventName(tomb) {
+    var event = "";
+    for (var i = 0; i < tomb.length; i++) {
+        if (i == 0)
+            event = tomb[i];
+        else
+            event = event + " " + tomb[i];
+    }
+    return event;
+} 
+
+function storeEvent(msg, e) {
+    var raw = splitMessage(msg.content);
+    var newevent = new Event();
+    newevent.eventId = 0;
+    newevent.eventName = 0;
+    newevent.eventDate = 0;
+    newevent.accepted = 0;
+    newevent.declined = 0;
+    newevent.tentative = 0;
+    newevent.madeBy = msg.author.username;
+    newevent.dateMade = msg;
+    
+}
 
 module.exports = {
     name: 'event',
     description: 'Event reminders can be set by this command',
     execute(message, args, Discord) {
         //
+        /*var events = [];
         var e = new Event();
         var tomb = e.splitMessage(message.content)
         min = tomb[tomb.length - 1];
@@ -53,13 +69,13 @@ module.exports = {
         mm = tomb[tomb.length - 1];
         tomb.pop();
         yy = tomb[tomb.length - 1];
-        tomb.pop();
+        tomb.pop();*/
         var event = e.getEventName(tomb);
-        
+        var d = new Date (message.createdTimestamp)
         const Embed = new Discord.MessageEmbed()
         .setAuthor('Event')
-        .setDescription('> ' + event)
-        .addField('Time', '> ' + yy + '.' + mm + '.' + dd + '. ' + hh + ':' + min)
+        .setDescription('> ' + d)
+        .addField('Time', '> ')
         .addFields (
             { name: 'Accepted', value: '> ACCEPTED_USERS\n> ACCEPTED_USERS', inline: true},
             { name: 'Declined', value: '> DECLINED_USERS', inline: true},
