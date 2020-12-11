c31026ea530f3f103f477ff1e0d0c96140eb2ade
Mátyás Fehér <mfeher96@gmail.com>
2020-12-11 21:03:47 +0100

    Update code.js

--- commands/code.js
+++ commands/code.js
@@ -1,1 +1,7 @@
-
+module.exports = {
+    name: 'code',
+    description: 'Can be used for sharing GitHub codes',
+    execute(message, args) {
+        message.reply("```py\n" + "print(teszt)\n" + "```");
+    }
+}
