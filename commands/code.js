a2973b5c7344cdcd587ab326f7fb274e30e18cbd
Mátyás Fehér <mfeher96@gmail.com>
2020-12-11 22:15:28 +0100

    Update code.js

--- commands/code.js
+++ commands/code.js
@@ -1,11 +1,9 @@
-const fetch = require('node-fetch');
-var url = "https://cdn.jsdelivr.net/gh/SparrowDivision/aoc2020/main/day_10/day10.py"
-fetch(url).then(d => d.text())
-
 module.exports = {
     name: 'code',
     description: 'Can be used for sharing GitHub codes',
-    execute(message, args) {
+    execute(message, args, fetch) {
+        var url = "https://cdn.jsdelivr.net/gh/SparrowDivision/aoc2020/main/day_10/day10.py";
+        fetch(url).then(d => d.text());
         message.reply("```py\n" + d + "```");
     }
 }
