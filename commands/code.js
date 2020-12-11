1582df02368eb1cf4cef28307566717c1ddf3fe9
Mátyás Fehér <mfeher96@gmail.com>
2020-12-11 21:17:47 +0100

    Update code.js

--- commands/code.js
+++ commands/code.js
@@ -1,10 +1,10 @@
-var url = "https://rawgithub.com/SparrowDivision/aoc2020/main/day_10/day10.py"
-fetch(url).then(d => d.text())
+//var url = "https://rawgithub.com/SparrowDivision/aoc2020/main/day_10/day10.py"
+//fetch(url).then(d => d.text())
 
 module.exports = {
     name: 'code',
     description: 'Can be used for sharing GitHub codes',
     execute(message, args) {
-        message.reply("```py\n" + d + "```");
+        message.reply("```py\n" + "d" + "```");
     }
 }
