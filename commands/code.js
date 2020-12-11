8f844ef1764943d03c3bcc3848d4095593c6e1cd
Mátyás Fehér <mfeher96@gmail.com>
2020-12-11 22:38:39 +0100

    Update code.js

--- commands/code.js
+++ commands/code.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'code',
     description: 'Can be used for sharing GitHub codes',
     execute(message, args, fetch) {
-        var url = "https://raw.githubusercontent.com/matthewkover/botus-primus/master/commands/code.js";
+        var url = "https://rawgithubusercontent.com/matthewkover/botus-primus/master/commands/code.js";
         d = fetch(url);
         d = d.text();
         message.reply("```py\n" + d + "```");
