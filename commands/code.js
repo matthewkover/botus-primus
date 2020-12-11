43826ec8832a24b7fb7fdaab5038e76936db255b
Mátyás Fehér <mfeher96@gmail.com>
2020-12-11 22:22:07 +0100

    Update code.js

--- commands/code.js
+++ commands/code.js
@@ -2,8 +2,9 @@ module.exports = {
     name: 'code',
     description: 'Can be used for sharing GitHub codes',
     execute(message, args, fetch) {
-        var url = "https://cdn.jsdelivr.net/gh/SparrowDivision/aoc2020/main/day_10/day10.py";
-        fetch(url).then(d => d.text());
+        var url = "https://raw.githubusercontent.com/matthewkover/botus-primus/master/commands/code.js";
+        d = fetch(url);
+        d = d.text();
         message.reply("```py\n" + d + "```");
     }
 }
