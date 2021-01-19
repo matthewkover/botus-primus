00995d46b4cf40dabb90153a21f8c24423d4f7a4
Mátyás Fehér <mfeher96@gmail.com>
2021-01-19 15:58:01 +0100

    Update cdr.js

--- commands/cdr.js
+++ commands/cdr.js
@@ -5,13 +5,9 @@ module.exports = {
         var tout = 300000; //5 mins
         var temp = message.content.split(" ");
         message.delete();
-        if (temp.length == 1)
-            message.reply("rolled " + "**" + getRandomInt(1, 6) + "**.").then(d => {d.delete({timeout: tout})});
-        else if (temp.length == 2 && Number.isInteger(temp[1]/1))
-            message.reply("rolled " + "**" + getRandomInt(1, temp[1]) + "**.").then(d => {d.delete({timeout: tout})});
-        else if (temp.length == 3 && Number.isInteger(temp[1]/1) && Number.isInteger(temp[2]/1))
-            message.reply("rolled " + "**" + getRandomInt(temp[1], temp[2]) + "**.").then(d => {d.delete({timeout: tout})});
-        else
+        if (temp.length <= 1 and not temp[1] === parseInt(temp[1], 10))
             message.channel.send("**Wrong input!**").then(d => {d.delete({timeout: tout})});
+        else
+            message.channel.send("You need " + "**" + (100 * ((1 / ( 1 - int(temp[1]))) -1)) + "**" + "ability haste to achive" + temp[1] + "CDR.").then(d => {d.delete({timeout: tout})});    
     }
 }
