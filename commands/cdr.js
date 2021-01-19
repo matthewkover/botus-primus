feea113a7866be5d0e74b6d61a589ab07397b1f6
Mátyás Fehér <mfeher96@gmail.com>
2021-01-19 15:47:48 +0100

    Create cdr.js

---
+++ commands/cdr.js
@@ -0,0 +1,17 @@
+module.exports = {
+    name: 'cdr',
+    description: 'This command rolls a dice.',
+    execute(message, args) {
+        var tout = 300000; //5 mins
+        var temp = message.content.split(" ");
+        message.delete();
+        if (temp.length == 1)
+            message.reply("rolled " + "**" + getRandomInt(1, 6) + "**.").then(d => {d.delete({timeout: tout})});
+        else if (temp.length == 2 && Number.isInteger(temp[1]/1))
+            message.reply("rolled " + "**" + getRandomInt(1, temp[1]) + "**.").then(d => {d.delete({timeout: tout})});
+        else if (temp.length == 3 && Number.isInteger(temp[1]/1) && Number.isInteger(temp[2]/1))
+            message.reply("rolled " + "**" + getRandomInt(temp[1], temp[2]) + "**.").then(d => {d.delete({timeout: tout})});
+        else
+            message.channel.send("**Wrong input!**").then(d => {d.delete({timeout: tout})});
+    }
+}
