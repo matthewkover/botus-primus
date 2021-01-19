42fa131a71dbce6678a7de424ce56421c181130b
Mátyás Fehér <mfeher96@gmail.com>
2021-01-19 17:29:00 +0100

    Create ah.js

---
+++ commands/ah.js
@@ -0,0 +1,19 @@
+function isInt(str) {
+  return !isNaN(str) && Number.isInteger(parseFloat(str));
+}
+
+module.exports = {
+    name: 'ah',
+    description: 'This command converts AH to CDR.',
+    execute(message, args) {
+        var tout = 300000; //5 mins
+        var temp = message.content.split(" ");
+        message.delete();
+        if (temp.length <= 1 || !isInt(temp[1]))
+            message.channel.send("**Wrong input!**").then(d => {d.delete({timeout: tout})});
+        else {
+            var cdr = 1 - 1 / (1 + (parseInt(temp[1], 10) / 100))
+            message.channel.send("**" + temp[1] + "** ability haste gives you " + "**" + parseInt(cdr, 10) + "**" + " cooldown reduction.").then(d => {d.delete({timeout: tout})});
+        }
+    }
+}
