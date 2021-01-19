dffc11400b1aa94eb4de74eb1ea7fe35002ab21f
Mátyás Fehér <mfeher96@gmail.com>
2021-01-19 16:09:49 +0100

    Update cdr.js

--- commands/cdr.js
+++ commands/cdr.js
@@ -1,3 +1,7 @@
+function isInt(str) {
+  return !isNaN(str) && Number.isInteger(parseFloat(str));
+}
+
 module.exports = {
     name: 'cdr',
     description: 'This converts CDR to AH.',
@@ -6,7 +10,7 @@ module.exports = {
         var temp = message.content.split(" ");
         message.delete();
         message.channel.send(temp).then(d => {d.delete({timeout: tout})});
-        if (temp.length == 1 || (temp[1] !== parseInt(temp[1], 10)))
+        if (temp.length <= 1 || !isInt(temp[2]))
             message.channel.send("**Wrong input!**").then(d => {d.delete({timeout: tout})});
         else
             message.channel.send("You need " + "**" + (100 * ((1 / ( 1 - int(temp[1]))) -1)) + "**" + "ability haste to achive" + temp[1] + "CDR.").then(d => {d.delete({timeout: tout})});    
