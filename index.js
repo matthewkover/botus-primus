41293d0483f7925109dc08084fcf77f286bc4692
Mate <matt.kovr@gmail.com>
2021-04-05 16:55:19 +0200

    prettier warning setup v1

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // LIBRARIES
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
-const ping = require('minecraft-server-util');
-const fs = require('fs');
-const mongoose = require('mongoose');
+const ping = require("minecraft-server-util");
+const fs = require("fs");
+const mongoose = require("mongoose");
 
 // BOT SETTINGS - Change these to deploy the bot locally
 
@@ -13,92 +13,95 @@ const DB_LOGIN = process.env.DB_LOGIN;
 const DB_SRV = `mongodb+srv://asdkhaa:${DB_LOGIN}@cluster0.dfwri.mongodb.net/botus-primus`;
 
 // CONNECT TO DATABASE
-mongoose.connect(DB_SRV, {
+mongoose
+  .connect(DB_SRV, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-}).then(()=>{
-    console.log('Connected to the database.');
-}).catch((err) => {
+  })
+  .then(() => {
+    console.log("Connected to the database.");
+  })
+  .catch((err) => {
     console.log(err);
-});
+  });
 
 var prev_status = false;
 
 client.commands = new Discord.Collection();
 
 // READ COMMAND DIRECTORY
-const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+const commandFiles = fs.readdirSync("./commands/").filter(file => file.endsWith(".js"));
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
     client.commands.set(command.name, command);
 }
 
 // STARTUP
-client.on('ready', () =>{
-    console.log('Bot is online.');
-    client.user.setActivity(`${PREFIX}help`, {type:'LISTENING'})
+client.on("ready", () =>{
+    console.log("Bot is online.");
+    client.user.setActivity(`${PREFIX}help`, {type:"LISTENING"})
     updateStatusText();
     setInterval(updateStatusText, 1 * 60 * 1000)
 })
 
 // STATUS TEXT QUERY
 function updateStatusText() {
-    ping('afb.servegame.com', 25565, (error, response) => {
-        console.log('Query was made.')
+    ping("afb.servegame.com", 25565, (error, response) => {
+        console.log("Query was made.")
         if (response !== null && prev_status == false) {
-            console.log('started')
-            //channel.send('Server has started')
+            console.log("started")
+            //channel.send("Server has started")
             prev_status = true
         }
         if (response == null && prev_status == true) {
-            console.log('stopped')
-            //channel.send('Server has stoped')
+            console.log("stopped")
+            //channel.send("Server has stoped")
             prev_status = false;
         }
     });
 }
 
 // CHAT COMMANDS
-client.on('message', message => {
-    if(!message.content.startsWith(PREFIX) || message.author.bot) return;
+client.on("message", message => {
+  if (!message.content.startsWith(PREFIX) || message.author.bot) return;
 
-    const args = message.content.slice(PREFIX.length).split(/ +/);
-    const command = args.shift().toLowerCase();
+const args = message.content.slice(PREFIX.length).split(/ +/);
+const command = args.shift().toLowerCase();
 
     switch(command){
-        case 'help':
-            client.commands.get('help').execute(message, args, Discord);
+        case "help":
+            client.commands.get("help").execute(message, args, Discord);
         break;
-        case 'ping':
-            client.commands.get('ping').execute(message, args);
+        case "ping":
+            client.commands.get("ping").execute(message, args);
         break;
-        case 'communism':
-            client.commands.get('communism').execute(message, args);
+        case "communism":
+            client.commands.get("communism").execute(message, args);
         break;
-        case 'role':
-            client.commands.get('role').execute(message, args);
+        case "role":
+            client.commands.get("role").execute(message, args);
         break;
-        case 'status':
-            client.commands.get('status').execute(message, args, ping, Discord);
+        case "status":
+            client.commands.get("status").execute(message, args, ping, Discord);
         break;
-        case 'event':
-            client.commands.get('event').execute(message, args, Discord);
+        case "event":
+            client.commands.get("event").execute(message, args, Discord);
         break;
-        case 'roll':
-            client.commands.get('roll').execute(message, args);
+        case "roll":
+            client.commands.get("roll").execute(message, args);
         break;
-        case 'championpool':
-            client.commands.get('championpool').execute(message, args);
+        case "championpool":
+            client.commands.get("championpool").execute(message, args);
         break;
-        case 'cdr':
-            client.commands.get('cdr').execute(message, args);
+        case "cdr":
+            client.commands.get("cdr").execute(message, args);
         break;
-        case 'ah':
-            client.commands.get('ah').execute(message, args);
+        case "ah":
+            client.commands.get("ah").execute(message, args);
         break;
-        case 'log':
+        case "log":
             console.log(message)
-            message.channel.send('Message parameters logged')
+            message.channel.send("Message parameters logged")
         break;
         default:
             message.channel.send(`The following command does not exists: **${PREFIX}` + command + `**\nWrite **${PREFIX}help** to see the list of things I can do.`);
