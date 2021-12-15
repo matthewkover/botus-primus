2fc05a9f0ffec8afd8a0860e92e825b697064b6e
Mátyás Fehér <mfeher96@gmail.com>
2021-12-15 15:11:03 +0100

    Update status.js

--- commands/status.js
+++ commands/status.js
@@ -2,7 +2,7 @@ module.exports = {
   name: "status",
   description: "This command pings the hardwired Minecraft channel and gives a status.",
   execute(message, args, ping, Discord) {
-    ping("afb.servegame.com", 25565, (error, response) => {
+    ping("afb.serveminecraft.com", 25565, (error, response) => {
       if (response == null) {
         message.delete()
         const Embed = new Discord.MessageEmbed()
